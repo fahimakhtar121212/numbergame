75076d687e12e152200e5c8900e74f46042dc91c
Fahim <44567578+fahimakhtar121212@users.noreply.github.com>
2024-03-24 03:33:27 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+
+let randomnumber = Math.floor(Math.random() * 6 + 1);
+
+let checkval = true;
+
+while (checkval == true) {
+    const answers = await inquirer.prompt([{
+        name: "usernumber",
+        type: "number",
+        message: "please guess a number between 1 to 6"
+    }]);
+    if (answers.usernumber === randomnumber) {
+        console.log("Conguratulation! you guess a right number.");
+    } else {
+        console.warn("Wrong number.");
+    }
+    const guessagain = await inquirer.prompt([{
+        name: "guessagain",
+        type: "confirm",
+        message: "\n Are you want to guess again ?"
+    }]);
+
+    if (guessagain.guessagain === false) {
+        checkval = false;
+    }
+}
